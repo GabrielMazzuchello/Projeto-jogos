e08592af4583d1e8cea5d123b5c1dd8b0ee92c9d
Gabriel Mazzuchello Dal Molin <94196035+GabrielMazzuchello@users.noreply.github.com>
2024-11-08 17:22:53 -0300

    Script do inimigo patrulhando finalizado

--- Jogo/Assets/Script/Enemy.cs
+++ Jogo/Assets/Script/Enemy.cs
@@ -10,6 +10,12 @@ public class Enemy : MonoBehaviour
     public Transform groundCheck;
     public LayerMask grondLayer;
     public bool facingRight = true;
+
+    public Transform player;
+    public float detectionRange;
+    public float minDistanceToPlayer = 1.5f;
+    private bool isChasing = false;
+
     void Start()
     {
         
@@ -18,31 +24,68 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        DetectPlayer();
+
+        if (isChasing)
+        {
+            MoveTowardsPlayer();
+        }
+        else
+        {
+            patrol();
+        }        
+    }
+
+    void patrol()
+    {
         transform.Translate(Vector2.right * speed * Time.deltaTime);
-        grond = Physics2D.Linecast(groundCheck.position, transform.position, grondLayer);
+
+        grond = Physics2D.Linecast(groundCheck.position, transform.position + Vector3.down * 0.1f, grondLayer);
         Debug.Log(grond);
 
         if (grond == false)
         {
             speed *= -1;
-        }
-
-        if (speed > 0 && !facingRight)
-        {
             flip();
         }
-        else if (speed < 0 && facingRight)
+
+    }
+
+    void flip ()
+    {
+        facingRight = !facingRight;
+        Vector3 Scale = transform.localScale;
+        Scale.x *= -1;
+        transform.localScale = Scale;
+    }
+
+    void DetectPlayer()
+    {
+        isChasing = Physics2D.OverlapCircle(transform.position, detectionRange, LayerMask.GetMask("Player"));
+
+        if (!isChasing && speed < 0 && facingRight || speed > 0 && !facingRight)
         {
             flip();
         }
         
     }
 
-    void flip ()
+    void MoveTowardsPlayer()
     {
-        facingRight = !false;
-        Vector3 Scale = transform.localScale;
-        Scale.x *= -1;
-        transform.localScale = Scale;
+        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+
+        if (distanceToPlayer > minDistanceToPlayer)
+        {
+            Vector2 direction = new Vector2(player.position.x - transform.position.x, 0).normalized;
+            
+            transform.Translate(direction * Mathf.Abs(speed) * Time.deltaTime);
+            
+            if ((player.position.x > transform.position.x && !facingRight) ||
+                (player.position.x < transform.position.x && facingRight))
+            {
+                flip();
+            }
+        }
     }
 }
+
